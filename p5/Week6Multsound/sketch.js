271df7287b967b18eae8ab514f14a75022e0b542
Rindo Child <53924846+rindo404@users.noreply.github.com>
2019-09-26 11:47:10 -0500

    sounds

--- p5/Week6Multsound/sketch.js
+++ p5/Week6Multsound/sketch.js
@@ -3,9 +3,9 @@ var myState = 0;
 
 
 function preload() {
-  song1 = loadSound('assets/donkey.mp3');
-  song2 = loadSound('assets/skate.mp3');
-  song3 = loadSound('assets/thrill.mp3');
+  song1 = loadSound('assets/Gomme/telecommunication.mp3');
+  song2 = loadSound('assets/wuthering.mp3');
+  song3 = loadSound('assets/gott.mp3');
 
   // set each song to loop and then immediately pause it so it doesn't play
 
